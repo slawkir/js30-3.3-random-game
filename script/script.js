86dd350ba08702be4df9358b0102bc8bbf717c97
slawkir <slawkir1984@gmail.com>
2023-10-09 06:23:50 +0400

    feat: add draw, move snake (Mon, Oct 09, 2023 06:23:00 AM)

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 'use strict';
 const gameField = document.getElementById('game-field');
 const context = gameField.getContext('2d');
+gameField.width = 600;
+gameField.height = 600;
 let tileSize = 20;
 let tileCount = gameField.width / tileSize;
 
@@ -16,9 +18,10 @@ let food = {
 
 let snake = [];
 let snakeHead = {
-  x: 60,
-  y: 60
+  x: 10,
+  y: 10
 };
+// snake = [snakeHead, snakeHead];
 
 let snakeTailCount = 1;
 
@@ -27,9 +30,93 @@ function drawGameField() {
   context.fillStyle = "black";
   context.fillRect(0, 0, gameField.width, gameField.height);
 }
-drawGameField();
+function drawSnake() {
+  context.fillStyle = "green";
+  for (let i = 0; i < snake.length; i++) {
+    // context.fillStyle = "green";
+    context.fillRect(snake[i].x * tileSize, snake[i].y * tileSize, tileSize, tileSize);
+
+    if (snake[i].x === snakeHead.x && snake[i].y === snakeHead.y) { 
+      snakeTailCount = 1;
+    }
+  }
+};
+
+function drawFood() { 
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, tileSize, tileSize);
+}
+
+function updateSnakeHead() { 
+  snakeHead.x += speed.x;
+  snakeHead.y += speed.y;
+
+  if (snakeHead.x < 0) {
+    snakeHead.x = tileCount - 1;
+  }
+  if (snakeHead.x > tileCount - 1) {
+    snakeHead.x = 0;
+  }
+  if (snakeHead.y < 0) {
+    snakeHead.y = tileCount - 1;
+  }
+  if (snakeHead.y > tileCount - 1) {
+    snakeHead.y = 0;
+  }
+}
+
+function updateSnakeBody() { 
+  snake.push({ x: snakeHead.x, y: snakeHead.y });
+  while (snake.length > snakeTailCount) {
+    snake.shift();
+  }
+}
+
+function eatFood() { 
+  if (food.x === snakeHead.x && food.y === snakeHead.y) {
+    snakeTailCount++;
 
 
-console.log(gameField.width);
-console.log(gameField.height);
-// document.addEventListener('')
+    food.x = Math.floor(Math.random() * tileCount);
+    food.y = Math.floor(Math.random() * tileCount);
+    // drawFood();
+  }
+}
+
+const keyDownHandlers = {
+  'ArrowLeft': () => {
+    speed.x = -1;
+    speed.y = 0;
+  },
+  'ArrowRight': () => {
+    speed.x = 1;
+    speed.y = 0;
+  },
+  'ArrowUp': () => {
+    speed.x = 0;
+    speed.y = -1;
+  },
+  'ArrowDown': () => {
+    speed.x = 0;
+    speed.y = 1;
+  },
+};
+
+function onKeyDown(event) { 
+  if (keyDownHandlers.hasOwnProperty(event.key)) { 
+    keyDownHandlers[event.key]();
+  }
+}
+
+function updateGame() { 
+  updateSnakeHead();
+  drawGameField();
+  drawSnake();
+  eatFood();
+  drawFood();
+  updateSnakeBody();
+}
+
+//events
+document.addEventListener('keydown', onKeyDown);
+setInterval(updateGame, 200);
